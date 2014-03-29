7131adcb49cf7290c3f1df8dcf351b50d3fddf1a
Kornel Lesiński <kornel@geekhood.net>
2014-03-29 16:19:18 +0000

    Error handling, usage info

--- undither.c
+++ undither.c
@@ -68,20 +68,27 @@ int main(int argc, char **argv) {
     size_t pngsize;
     LodePNGState state;
 
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s input-8bit.png output-32bit.png\n\nVersion 0.1, © 2014 Kornel Lesiński <kornel@geekhood.net>\nhttps://github.com/pornel/undither\n\n", argv[0]);
+        return 1;
+    }
+
     lodepng_state_init(&state);
     state.decoder.color_convert = 0;
     state.info_raw.colortype = LCT_PALETTE;
     state.info_raw.bitdepth = 8;
-    lodepng_load_file(&png, &pngsize, argv[1]);
-    error = lodepng_decode(&image, &width, &height, &state, png, pngsize);
+    error = lodepng_load_file(&png, &pngsize, argv[1]);
+    if (!error) {
+        error = lodepng_decode(&image, &width, &height, &state, png, pngsize);
+    }
     free(png);
     if (error) {
-        fprintf(stderr, "error %u: %s\n", error, lodepng_error_text(error));
-        return 1;
+        fprintf(stderr, "error when loading '%s': %s\n", argv[1], lodepng_error_text(error));
+        return error;
     }
 
     if (state.info_raw.bitdepth != 8) {
-        fprintf(stderr, "Fail! %d\n", state.info_raw.bitdepth);
+        fprintf(stderr, "Only 256-color images are supported\n");
         return 1;
     }
 
@@ -129,7 +136,11 @@ int main(int argc, char **argv) {
     lodepng_state_cleanup(&state);
     free(image);
 
-    lodepng_encode32_file("testout.png", (unsigned char*)out, width, height);
+    error = lodepng_encode32_file(argv[2], (unsigned char*)out, width, height);
+    if (error) {
+        fprintf(stderr, "error when saving '%s': %s\n", argv[2], lodepng_error_text(error));
+        return error;
+    }
 
     return 0;
 }
